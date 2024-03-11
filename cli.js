55fd02038ada17b41a816bee579739823f9d0ce7
vitormelods <vmsvitor20@gmail.com>
2024-03-11 11:06:59 -0300

    feat: adiçãoo do tipo de execução do arquivo

--- cli.js
+++ cli.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
 
-// Obtém o diretório atual
-const currentDir = process.cwd();
+// // Obtém o diretório atual
+// const currentDir = process.cwd();
 
-console.log('current', currentDir);
-console.log('current', process.argv[0]);
-console.log('current', process.argv[1]);
-console.log('current', process.argv[2]);
-console.log('antes', __dirname);
+// console.log('current', currentDir);
+// console.log('current', process.argv[0]);
+// console.log('current', process.argv[1]);
+// console.log('current', process.argv[2]);
+console.log('antes', __dirname + process.argv[2]);
 
 // Altera para o diretório atual
-process.chdir(currentDir);
+// process.chdir(currentDir);
 console.log('depois', __dirname);
 
 // Executa o script principal (index.js) do seu pacote
