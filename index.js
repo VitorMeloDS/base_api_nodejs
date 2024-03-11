9699d05acbd667e5fb2a2736f32099fea09d4a1b
vitormelods <vmsvitor20@gmail.com>
2024-03-11 19:45:17 -0300

    feat: adiçãoo do tipo de execução do arquivo

--- index.js
+++ index.js
@@ -57,25 +57,41 @@ async function copyDirectory(arg) {
   const currentDirectory = process.cwd() + '/' + arg;
   const sourceDirectory = path.join(__dirname);
 
-  fs.readdir(sourceDirectory, (err, files) => {
-    if (err) {
-      console.error('Error reading source directory:', err);
-      return;
+  const files = fs.readdir(sourceDirectory);
+  fs.mkdir(currentDirectory, { recursive: true });
+
+  for (const file of files) {
+    const sourcePath = path.join(sourceDirectory, file);
+    const destinationPath = path.join(currentDirectory, file);
+    const fileStat = fs.stat(sourcePath);
+    const notCopy = ['node_modules', projectName, 'index.js'];
+
+    if (fileStat.isDirectory() && !notCopy.includes(file)) {
+      await copyDirectory(sourcePath, destinationPath);
+    } else if (!notCopy.includes(file)) {
+      fs.copyFile(sourcePath, destinationPath);
     }
+  }
 
-    files.forEach(async (file) => {
-      const sourcePath = path.join(sourceDirectory, file);
-      const destinationPath = path.join(currentDirectory, file);
-      const notCopy = ['node_modules', arg, 'index.js', 'cli.js'];
-      const fileStat = fs.stat(sourcePath);
+  // fs.readdir(sourceDirectory, (err, files) => {
+  //   if (err) {
+  //     console.error('Error reading source directory:', err);
+  //     return;
+  //   }
 
-      if (fileStat.isDirectory() && !notCopy.includes(file)) {
-        await copyDirectory(sourcePath, destinationPath);
-      } else if (!notCopy.includes(file)) {
-        fs.copyFile(sourcePath, destinationPath);
-      }
-    });
-  });
+  //   files.forEach(async (file) => {
+  //     const sourcePath = path.join(sourceDirectory, file);
+  //     const destinationPath = path.join(currentDirectory, file);
+  //     const notCopy = ['node_modules', arg, 'index.js', 'cli.js'];
+  //     const fileStat = fs.stat(sourcePath);
+
+  //     if (fileStat.isDirectory() && !notCopy.includes(file)) {
+  //       await copyDirectory(sourcePath, destinationPath);
+  //     } else if (!notCopy.includes(file)) {
+  //       fs.copyFile(sourcePath, destinationPath);
+  //     }
+  //   });
+  // });
 }
 
 module.exports = copyDirectory;
