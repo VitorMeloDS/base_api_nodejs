25381b9b4ea8335c02b044709b53574028dde573
vitormelods <vmsvitor20@gmail.com>
2024-03-11 11:03:16 -0300

    feat: adiçãoo do tipo de execução do arquivo

--- cli.js
+++ cli.js
@@ -6,9 +6,11 @@ const currentDir = process.cwd();
 
 console.log('current', currentDir);
 console.log('current', process);
+console.log('antes', __dirname);
 
 // Altera para o diretório atual
 process.chdir(currentDir);
+console.log('depois', __dirname);
 
 // Executa o script principal (index.js) do seu pacote
 // execSync('node index.js', { stdio: 'inherit' });
